51e1acaf4253939d75f08118f94a80fa55b09293
Sandeep <sandeep.yadav@noosyntech.in>
2018-11-14 00:54:41 +0530

    Move micro-svc modules to different repos

---
+++ src/main/java/com/sandi/commonsvc/CommonSvcApplication.java
@@ -0,0 +1,12 @@
+package com.sandi.commonsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonSvcApplication.class, args);
+	}
+}
---
+++ src/main/java/com/sandi/commonsvc/config/SwaggerConfig.java
@@ -0,0 +1,56 @@
+package com.sandi.commonsvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+public abstract class SwaggerConfig extends WebMvcConfigurerAdapter{
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(getControllerPackage()))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        Contact contact = new Contact("Sandeep Yadav", "https://github.com/syadavdev", "sandi10.96@gmail.com");
+        return new ApiInfoBuilder()
+                .title(getTitle())
+                .description(getDescription())
+                .version("1.0.0")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .contact(contact)
+                .build();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/swagger-ui.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    protected abstract String getControllerPackage();
+
+    protected abstract String getTitle();
+
+    protected abstract String getDescription();
+
+}
